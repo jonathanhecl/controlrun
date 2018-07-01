730ce79ecc36f371a41ed6087fe14013e512bc43
Jonathan Hecl <jonathanhecl@users.noreply.github.com>
2018-07-01 16:03:39 -0300

    Now generates verification hash

--- controlrun.go
+++ controlrun.go
@@ -1,6 +1,8 @@
 package controlrun
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"errors"
 	"net/http"
 )
@@ -11,24 +13,48 @@ var (
 )
 
 // Setup the access endpoint
-func Set(firebase_server string, project_token string) {
+func Set(firebase_server string, project_token string) string {
+
+	_hash := sha1.New()
+
 	firehost = firebase_server
 	token = project_token
+
+	_hash.Write([]byte(firehost + token))
+	_sha := hex.EncodeToString(_hash.Sum(nil))
+
+	return _sha
+
 }
 
 // Check if have access
-func Run() (bool, error) {
+func Run(hash ...string) error {
+
 	if len(firehost) == 0 || len(token) == 0 {
-		return false, errors.New("Setup missing!")
+		return errors.New("Setup missing!")
 	}
+
+	if len(hash) > 0 {
+		_hash := sha1.New()
+		_hash.Write([]byte(firehost + token))
+		_sha := hex.EncodeToString(_hash.Sum(nil))
+
+		if _sha != hash[0] {
+			return errors.New("Permission denied!")
+		}
+	}
+
 	resp, err := http.Get("https://" + firehost + ".firebaseio.com/" + token + ".json")
 	if err != nil {
-		return false, err
+		return err
 	}
+
 	defer resp.Body.Close()
 
 	if resp.StatusCode == http.StatusOK {
-		return true, nil
+		return nil
 	}
-	return false, nil
+
+	return errors.New("Permission denied!")
+
 }
